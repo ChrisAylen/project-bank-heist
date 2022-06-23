68a230f9268f96c4ecfc0123059c478e29d75049
shingchan95 <chanhei888@gmail.com>
2022-06-23 19:43:33 +0100

    add accountRoutes

---
+++ controllers/API/accountRoutes.js
@@ -0,0 +1,41 @@
+const router = require('express').Router();
+const { Account } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newAccount = await Account.create({
+      account_name:req.params.account,
+      balance:req.params.balance,
+      interest_rate:req.params.interest_rate,
+      user_id: req.session.user_id,
+    });
+    
+
+    res.status(200).json(newAccount);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const newAccount = await Account.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!newAccount) {
+      res.status(404).json({ message: 'No account found with this id!' });
+      return;
+    }
+
+    res.status(200).json(newAccount);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
--- controllers/API/index.js
+++ controllers/API/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
-
+const accountRoutes = require('./accountRoutes');
 
 router.use('/user', userRoutes);
+router.use('/account', accountRoutes);
 
 
 module.exports = router;
